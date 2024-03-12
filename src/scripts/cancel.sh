5a7570b4d8d8fbe9c031a5c8f2ae36b4e022bd3b
e-grapp <ervin@circleci.com>
2024-03-12 13:20:42 -0600

    deleted cancel tests

--- src/scripts/cancel.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# This example uses envsubst to support variable substitution in the string parameter type.
-# https://circleci.com/docs/orbs-best-practices/#accepting-parameters-as-strings-or-environment-variables
-echo "Cancel workflow"
-curl -s -H 'Accept: application/json' -X POST https://circleci.com/api/v2/workflow/"${CIRCLE_WORKFLOW_ID}"/cancel?circle-token="${<< parameters.api_key >>}"
